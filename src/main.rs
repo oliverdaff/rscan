6539c5d6a8211e40ff3db300750927e3bc08629b
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-07-12 20:59:49 +1000

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,20 @@
-use tokio::net::TcpStream;
 use futures::{stream, StreamExt};
-
-
+use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() {
-
     stream::iter(1i32..1024)
-    .map(|p| {
-        async move {
+        .map(|p| async move {
             TcpStream::connect(format!("scanme.nmap.org:{}", p))
-            .await
-            .map(|x|(x, p))
-        }
-    })
-    .buffer_unordered(100)
-    .for_each(|c| async {
-        match c {
-            Ok((_, p)) => println!("{} open", p),
-            Err(_err) => ()
-        }
-    }).await;
+                .await
+                .map(|x| (x, p))
+        })
+        .buffer_unordered(100)
+        .for_each(|c| async {
+            match c {
+                Ok((_, p)) => println!("{} open", p),
+                Err(_err) => (),
+            }
+        })
+        .await;
 }
