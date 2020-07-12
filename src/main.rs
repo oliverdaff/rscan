b25a3e236ade37264e61ef6980e4b8a4980007fa
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-07-12 10:52:32 +1000

    added slow basic tcp scan

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpStream;
+
 fn main() {
-    println!("Hello, world!");
+    for i in 0..1024 {
+        match TcpStream::connect(format!("scanme.nmap.org:{}", i)){
+            Ok(_stream) => println!("{} open", i),
+            Err(_err) => ()
+        }
+
+    }
 }
