02f451701ef720882246daee45a7de44ee917479
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-07-13 16:09:38 +1000

    collect results

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
-use futures::{stream, StreamExt};
+use futures::stream::{self, StreamExt};
 use tokio::net::TcpStream;
+
+#[derive(Debug, Clone, PartialOrd, PartialEq, Eq, Ord)]
+enum PortResult {
+    Open(u16),
+    Closed(u16)
+}
+
+impl PortResult {
+    pub fn is_open(&self) -> bool {
+        matches!(*self, PortResult::Open(_))
+    }
+
+}
 
 #[tokio::main]
 async fn main() {
-    stream::iter(1i32..1024)
-        .map(|p| async move {
-            TcpStream::connect(format!("scanme.nmap.org:{}", p))
-                .await
-                .map(|x| (x, p))
-        })
-        .buffer_unordered(100)
-        .for_each(|c| async {
-            match c {
-                Ok((_, p)) => println!("{} open", p),
-                Err(_err) => (),
-            }
-        })
-        .await;
+    let x = stream::iter(1u16..100).map(|p| open_connection(p));
+    let results = x.buffer_unordered(100).collect::<Vec<_>>().await;
+    let (mut open, _): (Vec<_>, Vec<_>) = results.iter().partition(|x|PortResult::is_open(x));
+
+    open.sort();
+
+    println!("Open Ports:");
+    for port in open {
+        match port {
+            PortResult::Open(p) => println!("{}", p),
+            _ => ()
+        }
+    }
+}
+
+async fn open_connection(port: u16) -> PortResult {
+    TcpStream::connect(format!("scanme.nmap.org:{}", port)).await
+    .map_or(PortResult::Closed(port), |_| PortResult::Open(port))
+
 }
